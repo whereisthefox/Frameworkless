292dc269f184b1dd99da02c63cbf885632eb5a61
perkx166 <perkx166@gmail.com>
2017-04-29 21:30:30 +0000

    get data form body ready

--- foxtail/routes.php
+++ foxtail/routes.php
@@ -4,6 +4,6 @@ return [
     ['GET', '/', ['Frameworkless\Controllers\IndexController', 'index']],
     ['GET', '/exception', ['Frameworkless\Controllers\IndexController', 'exception']],
     ['GET', '/greet/{name}', ['Frameworkless\Controllers\GreetController', 'greet']],
-    ['GET', '/api', ['Frameworkless\Controllers\ApiController', 'getData']],
+    ['POST', '/api', ['Frameworkless\Controllers\ApiController', 'getData']],
     ['GET', '/test-api', ['Frameworkless\Controllers\ApiController', 'sendData']],
 ];
--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -19,33 +19,49 @@ class ApiController
     {
         $request = Request::createFromGlobals();
         if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-            $data = json_decode($request->getContent(), true);
-            // $data = $request->getContent();
-            $request->request->replace(is_array($data) ? $data : array());
+	    $data = $request->getContent();
+            $data2 = json_decode($data, true);
+            $request->request->replace(is_array($data2) ? $data2 : array());
         } else {
-            $data = $request->getContent();
+            $data2 = $request->getContent();
         }
 
+	var_dump($data2);
         $response = new Response(
-            $data,
+            'Upload sucessfull!',
             Response::HTTP_OK,
-            array('Content-Type' => 'text/html')
+            array('Content-Type' => 'application/json')
         );
 
         $response->send();
     }
 
+/**
+  * Usedfortesting
+  *
+  */
     public function sendData()
     {
         $r = Request::createFromGlobals();
         $r->headers->set(('Content-Type'), 'application/json');
-        $r->body = '{
-            "aaa": "bbb",
-            "ccc": "ddd"
-        }';
-
-        return($this->getData());
+        $r->request->set(('body'), '{"coord":{"lon":139,"lat":35},
+"sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
+"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+"wind":{"speed":7.31,"deg":187.002},
+"rain":{"3h":0},
+"clouds":{"all":92},
+"dt":1369824698,
+"id":1851632,
+"name":"Shuzenji",
+"cod":200}');
+        return($this->getData($r));
     }
+
+   public function apendData()
+   {
+   }
+
 }
 
  ?>
